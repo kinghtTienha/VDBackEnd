d673ce10001b91da69288919e0a68a8d028c68f8
Hoang Anh Tien <hoanganhtien@Hoangs-MacBook-Pro.local>
2021-02-08 13:45:51 +0700

    init project !

---
+++ VDDemo/src/main/java/com/vd/demo/DemoVdApplication.java
@@ -0,0 +1,13 @@
+package com.vd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoVdApplication.class, args);
+    }
+
+}
---
+++ VDDemo/src/test/java/com/vd/demo/DemoVdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
